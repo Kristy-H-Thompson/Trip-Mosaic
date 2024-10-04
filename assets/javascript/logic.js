c19fb7c58ed57f98a31c834b023b359c248926f2
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-10-03 19:41:54 -0600

    javascript formatting

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,11 +6,11 @@ Makes the nav bar responsive with a hamburger menu
 ------------------------------------------------------------------------------------------------------------ 
 */
 
-    // Define some constants
-      const menuButton = document.getElementById('menu-button');
-      const mobileMenu = document.getElementById('mobile-menu');
-    
-    // When hamburger menu icon is clicked, toggle the nav box
-      menuButton.addEventListener('click', () => {
-          mobileMenu.classList.toggle('hidden');
-      });
+// Define some constants
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+// When hamburger menu icon is clicked, toggle the nav box
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
